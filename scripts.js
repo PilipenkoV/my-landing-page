027b1f72b06642644a9b896ce433414f9c0365bc
Vyacheslav <111230803+PilipenkoV@users.noreply.github.com>
2023-03-21 01:21:48 +0200

    Add block education

--- scripts.js
+++ scripts.js
@@ -62,6 +62,24 @@ scrollUp.addEventListener('click', () => {
 	});
 })
 
+// ! ===== accordion in 'about me' section =====
+
+document.querySelectorAll('.accordion-item__trigger').forEach((item) =>
+	item.addEventListener('click', () => {
+		const parent = item.parentNode;
+
+		if (parent.classList.contains('accordion-item--active')) {
+			parent.classList.remove('accordion-item--active');
+		} else {
+			document.querySelectorAll('.accordion-item').forEach((child) => child.classList.remove('accordion-item--active'))
+
+			parent.classList.add('accordion-item--active');
+		}
+
+
+	})
+)
+
 // ! ===== copyright date update =====
 
 const year = document.querySelector('#current-year');
